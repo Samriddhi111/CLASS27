af14e26f102ebc59a158208e4fb1c54dd79631b5
Samriddhi111 <you@example.com>
2020-10-15 14:50:46 +0530

    constrained

--- sketch.js
+++ sketch.js
@@ -1,10 +1,13 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var constrainedLog;
+
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -15,7 +18,7 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
-
+    constrainedLog = new Log(230,180,80,PI/2);
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
 
@@ -35,6 +38,8 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+//chain = new Chain(bird.body,constrainedLog.body);
+chain=new Chain(bird.body,log3.body);
 
 }
 
@@ -58,7 +63,11 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
-
+    constrainedLog.display();
     bird.display();
     platform.display();
+    chain.display();
+
+    //line (x1,y1,x2,y2)
+   
 }
